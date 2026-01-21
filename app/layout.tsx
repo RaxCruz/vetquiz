bd71fbc82b4edffd11d861d971254d0e1d8a480d
祐嘉 <39622968+RaxCruz@users.noreply.github.com>
2026-01-21 15:01:25 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark">
+    <html lang="zh-TW" style={{ colorScheme: 'dark' }}>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
